e8aba526207e135fd1190d3e4690259b2eef528c
szyper <zskgit@gmail.com>
2023-09-12 15:55:00 +0200

    typeadd new projects

---
+++ po/gr_1/2_project/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_project
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //komentarz wjednej linii
+
+      /* komentarz
+       * w kilku liniach
+       * w kilku liniach
+       */
+
+      Console.Write("C# - ");
+      Console.WriteLine("język programowania");
+      Console.WriteLine("Nowa linia");
+      Console.Clear(); //czyszczenie ekranu
+
+      //zmienne (deklaracja i inicjalizacja)
+      int i = 10;
+      Console.WriteLine("Wartość zmiennej i: " + i);
+
+
+      Console.WriteLine("Wartość zmiennej i: {0}", i);
+
+      System.Int64 j = 100;
+      Console.WriteLine("Wartość zmiennej j: {0}, a wartość zmiennej i: {1}", j, i);
+
+      //System.Int => 16, 32, 64
+
+      long l = 20;
+      Console.WriteLine("Zmienna l: {0}", l);
+
+      byte b = 255; //0 - 255
+      sbyte sb = -128; //-128 - 127
+
+      short s = -30000; //-32768 - 32767
+
+      int i1 = 1000000; //-2 147 483 648 - 2 147 483 647  
+
+      //dokończyć typy m.in. uint
+
+      Console.ReadKey();
+    }
+  }
+}
---
+++ po/gr_2/2_project_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_project_2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      //komentarz w jednej linii
+
+      /*
+       * komentarz w 
+       * wielu liniach
+      */
+
+      Console.Write("Pierwszy program ");
+      Console.WriteLine("w języku C#");
+      Console.WriteLine("test");
+      Console.Clear();
+
+      // typy danych
+      /*
+       * int 16, 32, 64
+       * byte 0 - 255
+       * sbyte -128 - 127
+       * short -32768 - 32767
+       * int -2 147 483 648 - 2 147 483 647 
+      */
+
+      int i; // deklaracja zmiennej i
+      int i1 = 10; //deklaracja z jednoczesną inicjalizacją zmiennej i 
+
+      Console.WriteLine("Zmienna i1 wynosi: " + i1);
+
+      System.Int32 i2 = 100;
+
+      Console.WriteLine("Zmienna i1 wynosi: {0}, zmienna i2 wynosi: {1}", i1, i2);
+      Console.WriteLine($"Zmienna i1 wynosi: {i1}, zmienna i2 wynosi: {i2}");
+
+      byte b = 100;
+      sbyte sb = -10;
+      short s = -20000;
+
+      //dokończyć uint i pozostałe typy danych
+
+      Console.ReadKey();
+    }
+  }
+}
