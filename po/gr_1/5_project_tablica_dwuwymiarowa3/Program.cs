c49a05840f408b6c8d3bfd36e6f9858e2075f11c
szyper <31780468+szyper@users.noreply.github.com>
2023-11-16 07:32:24 +0100

    t19

---
+++ po/gr_1/5_project_tablica_dwuwymiarowa3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5_project_tablica_dwuwymiarowa3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ po/gr_1/5_project_tablice_2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _5_project_tablice_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tabA = CreateArray("tabA");
+            if (tabA != null)
+            {
+                SetArray(tabA, "tabA");
+                Console.WriteLine();
+                DisplayArray(tabA, "tabA");
+            }
+        }
+
+        public static int[] CreateArray(string name)
+        {
+            Console.Write($"Podaj rozmiar tablicy <0 ; {int.MaxValue}> {name}:");
+            int size = 0;
+            try
+            {
+                size = int.Parse(Console.ReadLine());
+                if (size <= 0) throw new ArgumentException("rozmiar tablicy musi być dodatni");
+            }
+            catch(ArgumentException e )
+            {
+                Console.WriteLine("\nWystąpił błąd: {0}\n\n", e.Message);
+                return null;
+            }
+            
+            int[] array = new int[size];
+            return array;
+        }
+       
+        public static void SetArray(int[] array, string name)
+        {
+            Console.WriteLine($"\nPodaj elementy tablicy {name}");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"Podaj element tablicy {i+1}:");
+                try
+                {
+                    array[i] = int.Parse(Console.ReadLine());
+                }
+                catch (Exception e )
+                {
+                    Console.WriteLine("\nWystąpił błąd: {0}\n", e.Message);
+                    i--;
+                }
+            }
+        }
+        public static void DisplayArray(int[] array, string name)
+        {
+            Console.WriteLine($"\nWyświetlenie zawartości tablicy {name}:");
+            int i = 0;
+            foreach (var element in array)
+            {
+                Console.WriteLine($"{name}[{i}]: {element}");
+                i++;
+            }
+        }
+    }
+}
